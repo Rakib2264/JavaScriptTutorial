bf56f6bd4bffa442c3842abecaa26f17424981ea
Rakib2264 <rakibul15-2264@diu.edu.bd>
2022-06-18 18:13:42 +0600

    posiable
    posiable

--- tut.js
+++ tut.js
@@ -1,3 +1,8 @@
 console.log({Rakib:'this',marks:34});
 console.table({Rakib:'this',marks:34});
 console.warn('Rakib');
+
+console.timeEnd('time end')
+console.assert(52>189,'this is not posiable');
+
+document.colore="lightblue";
